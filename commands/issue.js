d418dfef52299209dfa99a1692969606a5a922ce
似鳥ちか <chika@nitadori.net>
2023-05-24 22:30:35 +0900

    こうしんだよ

--- commands/issue.js
+++ commands/issue.js
@@ -12,15 +12,15 @@ module.exports = {
                 footer: {
                     text: "焚き火ちゃん",
                 },
-                title: "[Issue](https://github.com/NitadoriNetwork/takibichan/issues)",
+                title: "Issue",
                 fields: [
                     {name: "機能名", value: `ステータス`},
-                    {name: "[新規参加者へのロール自動付与機能の実装](https://github.com/NitadoriNetwork/takibichan/issues/1)", value: `Complete`, inline: true},
-                    {name: "[自動削除機能が動かない問題の修正](https://github.com/NitadoriNetwork/takibichan/issues/2)", value: `Complete`, inline: true},
-                    {name: "[通話通知発信拒否設定の改善](https://github.com/NitadoriNetwork/takibichan/issues/8)", value: `Suspended`, inline: true},
-                    {name: "[Googleカレンダー関連機能](https://github.com/NitadoriNetwork/takibichan/issues/10)", value: `Suspended`, inline: true},
-                    {name: "[アナウンス機能の改善](https://github.com/NitadoriNetwork/takibichan/issues/11)", value: `Suspended`, inline: true},
-                    {name: "[ガイド系コマンドの修正](https://github.com/NitadoriNetwork/takibichan/issues/13)", value: `Pending`, inline: true}
+                    {name: "新規参加者へのロール自動付与機能の実装", value: `[Complete](https://github.com/NitadoriNetwork/takibichan/issues/1)`, inline: true},
+                    {name: "自動削除機能が動かない問題の修正", value: `[Complete](https://github.com/NitadoriNetwork/takibichan/issues/2)`, inline: true},
+                    {name: "通話通知発信拒否設定の改善", value: `[Suspended](https://github.com/NitadoriNetwork/takibichan/issues/8)`, inline: true},
+                    {name: "Googleカレンダー関連機能", value: `[Suspended](https://github.com/NitadoriNetwork/takibichan/issues/10)`, inline: true},
+                    {name: "アナウンス機能の改善", value: `[Suspended](https://github.com/NitadoriNetwork/takibichan/issues/11)`, inline: true},
+                    {name: "ガイド系コマンドの修正", value: `[Pending](https://github.com/NitadoriNetwork/takibichan/issues/13)`, inline: true}
                 ]
                 }],
             })
