062cecddd1ee834ab9766066ce490112cc7bebc4
似鳥ちか <chika@nitadori.jp>
2023-06-12 23:29:35 +0900

    自動的に年齢確認をする機能の実装 Fixes #17

--- commands/age-auth.js
+++ commands/age-auth.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, T
 const dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 const webhookClient = new WebhookClient({ url: process.env.AAWEBHOOK});
+const rule = /^[0-9]{8}$/
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -22,42 +23,68 @@ module.exports = {
 		interaction.awaitModalSubmit({ filter, time: 100000000 })
 			.then(async mInteraction => {
 				const title = mInteraction.fields.getTextInputValue('year');
-				await mInteraction.reply({
+				let result = rule.test(title);
+				if ( result === true ) {
+					await mInteraction.reply({
+							embeds: [{
+							color: 0xF00035,
+							timestamp: new Date(),
+							footer: {
+								text: "焚き火ちゃん",
+							},
+							title: "申請完了",
+							fields: [
+                    	        {name: "入力内容", value: title},
+                    	        {name: "申請者", value: `${interaction.user.username}`}
+                        	]
+							}],
+					ephemeral: true
+						});
+						let date = new Date();
+						let nowy = date.getFullYear();
+						let nowm = date.getMonth();
+						let nowd = date.getDate();
+						let nowdate = Number(`${nowy}${nowm}${nowd}`);
+						if ( title >= nowdate) {
+							interaction.member.roles.add('1041387252560969738');
+						} 
+					webhookClient.send({
+						username: '焚き火ちゃん',
+						avatarURL: process.env.AVATERURL,
+            	        embeds: [{
+	    	        	    color: 0xF00035,
+    	    	        	timestamp: new Date(),
+	    		            footer: {
+	    		                text: "焚き火ちゃん",
+    	        	        },
+        	        	    thumbnail: {
+            		            url: interaction.user.avatarURL(),
+                	    },
+						title: "年齢申請フォーム",
+						fields: [
+        	                {name: "入力内容", value: title},
+            	            {name: "申請者", value: `${interaction.user.username}`},
+                	        {name: "ID", value: `${interaction.user.id}`}
+	                    ]
+					}],
+        	        });
+				} else {
+					await mInteraction.reply({
 						embeds: [{
 						color: 0xF00035,
 						timestamp: new Date(),
 						footer: {
 							text: "焚き火ちゃん",
 						},
-						title: "申請完了",
+						title: "Error",
 						fields: [
-                            {name: "入力内容", value: title},
-                            {name: "申請者", value: `${interaction.user.username}`}
-                        ]
+							{name: "Error", value: `入力された内容は半角数字8桁ではありません。`},
+						]
 						}],
-				ephemeral: true
+					ephemeral: true
 					});
-			webhookClient.send({
-				username: '焚き火ちゃん',
-				avatarURL: process.env.AVATERURL,
-                    embeds: [{
-                    color: 0xF00035,
-                    timestamp: new Date(),
-                    footer: {
-                        text: "焚き火ちゃん",
-                    },
-                    thumbnail: {
-                        url: interaction.user.avatarURL(),
-                    },
-					title: "年齢申請フォーム",
-					fields: [
-                        {name: "入力内容", value: title},
-                        {name: "申請者", value: `${interaction.user.username}`},
-                        {name: "ID", value: `${interaction.user.id}`}
-                    ]
-				}],
-                });
-			})
-			.catch(console.error);
-	},
-};
+				}
+				})
+				.catch(console.error);
+		},
+};	
