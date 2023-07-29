08271f71ead78d9e97fd80ca72600d784a68be3a
Nitadori Chika <chika@nitadori.jp>
2023-07-30 01:07:32 +0900

    コマンド関係のコードも削除

--- index.js
+++ index.js
@@ -8,36 +8,5 @@ dotenv.config({ path: './.env' });
 
 const client = new Client({ intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent], partials: [Partials.Message, Partials.Channel, Partials.Reaction] });
 
-
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-        client.commands.set(command.data.name, command);
-}
-
-client.once(Events.ClientReady, () => {
-	console.log('Ready!!'); // 起動時に"Ready!!"とコンソールに出力する
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.followUp({ content: 'エラーが起きてしまいました...', ephemeral: true});
-    }
-});
-
 //ログイン
 client.login(process.env.TOKEN); 
